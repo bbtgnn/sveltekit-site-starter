4a07c9e80d203412af88a797a0f0f57689c3456a
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-05-22 05:12:01 +0200

    sutff

---
+++ src/lib/components/partials/navbar-link.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	type Attributi = {
+		url: string;
+		testo: string;
+	};
+
+	let a: Attributi = $props();
+</script>
+
+<a
+	href={a.url}
+	class="bg-primary text-primary-foreground hover:bg-secondary hover:text-secondary-foreground"
+>
+	{a.testo}
+</a>
--- src/lib/components/partials/navbar.svelte
+++ src/lib/components/partials/navbar.svelte
@@ -1,3 +1,12 @@
-<nav class="bg-primary text-primary-foreground">
-	<p>ciao</p>
+<script>
+	import NavbarLink from './navbar-link.svelte';
+</script>
+
+<nav class="bg-primary text-primary-foreground flex justify-between">
+	<NavbarLink url="/" testo="Home" />
+
+	<div>
+		<NavbarLink url="/chi-siamo" testo="Chi siamo" />
+		<NavbarLink url="/progetti" testo="Progetti" />
+	</div>
 </nav>
