0b7f3644ea0cce87c5cae0fa5ab1310a098d734e
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-05-28 18:20:57 +0200

    Update navbar-link.svelte

--- src/lib/components/partials/navbar-link.svelte
+++ src/lib/components/partials/navbar-link.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { base } from '$app/paths';
+
 	type Attributi = {
 		url: string;
 		testo: string;
@@ -8,7 +10,7 @@
 </script>
 
 <a
-	href={a.url}
+	href="{base}{a.url}"
 	class="bg-primary text-primary-foreground hover:bg-secondary hover:text-secondary-foreground"
 >
 	{a.testo}
